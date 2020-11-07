9933117d8ff7f090ba39045d0c8d4ece5ae81a63
SergeJawhari <60945353+SergeJawhari@users.noreply.github.com>
2020-11-06 22:07:22 -0800

    grabbing ip

--- Logging Code/Logging Practice Code/LoggingPractice2.0App/LoggingPractice2.0/Program.cs
+++ Logging Code/Logging Practice Code/LoggingPractice2.0App/LoggingPractice2.0/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -12,6 +13,18 @@ namespace LoggingPractice2._0
 {
     public class Program
     {
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if(ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
         public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
