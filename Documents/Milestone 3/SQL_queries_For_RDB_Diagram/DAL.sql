19f043c34e0740fbabb573abe1d9a098729819ce
Farhatzee <zeinabfarhat40@gmail.com>
2020-12-10 12:39:25 -0800

    updated autobuild SQL part 6 + the DAL sql for CODE

---
+++ Documents/Milestone 3/SQL_queries_For_RDB_Diagram/DAL.sql
@@ -0,0 +1,45 @@
+
+USE AutoBuild_V3
+
+DROP TABLE IF EXISTS userAccounts;
+DROP TABLE IF EXISTS userCredentials;
+
+
+ -- userAccounts (1..1) -> UserCredentials (1..1)
+ CREATE TABLE userAccounts(
+
+ userID BIGINT NOT NULL IDENTITY(0010000,2),
+ username varchar(30),
+ email varchar(30),
+ passwordHash varchar(20),
+ firstName varchar(20),
+ lastName varchar(20),
+ modifiedAt DATETIME,
+ modifiedBy DATETIME,
+-- primary key
+CONSTRAINT userAccounts_PK PRIMARY KEY(userID)
+
+ );
+ 
+ -- userAccounts (1..1) -> UserCredentials (1..1)
+  CREATE TABLE userCredentials(
+
+ userCredID BIGINT NOT NULL, 
+ username varchar(30),
+ passwordHash varchar(20),
+ modifiedAt DATETIME,
+ modifiedBy DATETIME,
+ -- creating a enum
+ -- accountTypes represented as an enumeration:
+ roley varchar(15) NOT NULL CHECK (roley IN ('BASIC' , 'ADMIN', 'DEVOLOPER', 'VENDOR')),
+
+-- primary key
+CONSTRAINT userCredentials_PK PRIMARY KEY(userCredID),
+
+--CONSTRAINT userCredentials_CK UNIQUE (userCredID),
+
+CONSTRAINT	userCredentials_UserAccount_FK FOREIGN KEY
+(userCredID) REFERENCES userAccounts(userID)
+
+ );
+
