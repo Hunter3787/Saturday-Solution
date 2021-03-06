26810108f101cdd38fcce0b81b4cec4d40fcb165
Nick Marshall-Eminger <hunter3787@gmail.com>
2021-03-06 08:32:47 -0800

    Small changes

---
+++ AutoBuildApp/AutoBuildApp_v2/AutoBuildApp.React/Controllers/apiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AutoBuildApp.BusinessLayer;
+using System.Net;
+using System.Net.Http;
+
+namespace AutoBuildApp.Controllers
+{
+/*
+    [EnableCors("CorsPolicy")]
+    [Route("[controller]")]
+    [ApiController]
+    public class AutoBuildUserManager : ControllerBase
+    {
+           
+    }
+
+    [HttpGet]
+    public void GetUserAccount()
+    {
+
+    }
+
+    [HttpPost]
+    public void CreateUserAccount() { }
+*/
+
+}
--- AutoBuildApp/AutoBuildApp_v2/AutoBuildApp.React/Controllers/registrationController.cs
+++
@@ -1,2 +0,0 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Cors;
