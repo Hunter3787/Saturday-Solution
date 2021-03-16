72b8c000bb0d5462eb8004ab1901a0ed73e0967e
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-03-15 21:30:49 -0700

    added query

---
+++ Async Logging/Async Logging/LogQuery.sql
@@ -0,0 +1,21 @@
+﻿USE DB;
+
+DROP TABLE IF EXISTS logs;
+
+CREATE TABLE logs(
+
+logID INTEGER NOT NULL IDENTITY(0030000,2), -- PRIMARY KEY,
+--creationDate DATETIME,
+--event Varchar(20), --example 192/168/1/1
+message text, -- 
+loglevel Varchar(20),
+dateTime Varchar(50),
+
+-- primary key
+CONSTRAINT logs_PK PRIMARY KEY(logID,loglevel),
+
+);
+
+INSERT INTO logs(message, loglevel, dateTime) VALUES ('THIS IS A TEST', '0', '2019');
+
+select * from logs;
