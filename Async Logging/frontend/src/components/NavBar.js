21ca08bab178fc8c68f22193d736d058a19ce031
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-03-18 17:18:09 -0700

    changed everything to react.js
    currently learning react.js, want to use it for full integration testing.

---
+++ Async Logging/frontend/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import './NavBar.css';
+
+function NavBar(){
+    return(
+        <div>
+            <body>
+                <nav>
+                    <h2>Reviews and Ratings</h2>
+                </nav>
+            </body>
+        </div>
+    );
+}
+
+export default NavBar;
