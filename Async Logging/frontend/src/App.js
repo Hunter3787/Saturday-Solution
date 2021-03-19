076a2f11b2e7b4ac351b29118a00fe00b654073e
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-03-18 18:43:22 -0700

    simple form created

--- Async Logging/frontend/src/App.js
+++ Async Logging/frontend/src/App.js
@@ -1,12 +1,42 @@
 import React from "react";
 import './App.css';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Container,Row,Col, Button, Form, InputGroup} from 'react-bootstrap';
 
 function App(){
-
   return(
-    <div className ="app">
+    <div className ="App">
       <NavBar />
+      <header className = "App-header">
+        <Container>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group controlId="WriteReview">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text>Enter a Review</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <Form.Control as="textarea" aria-label="With textarea" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="Submit">
+                  <Button type="submit">Submit Review</Button>
+                </Form.Group>
+              </Col>
+            </Row>
+          </Form>
+        </Container> 
+        {/* <Form>
+          <Form.Group>
+            <Form.Label>Review</Form.Label>
+            <Form.Control type="email" placeholder="hello@gmail.com" />
+            <Form.Text className="text-muted">We wont share your email</Form.Text>
+          </Form.Group>
+        </Form> */}
+      </header>     
     </div>
   );
 }
