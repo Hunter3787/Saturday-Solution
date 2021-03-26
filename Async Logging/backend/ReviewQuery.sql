366b4755f4bf5edd1eea40a828864655fea2c486
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-03-26 03:13:09 -0700

    updated reviews query

--- Async Logging/backend/ReviewQuery.sql
+++ Async Logging/backend/ReviewQuery.sql
@@ -4,16 +4,20 @@ DROP TABLE IF EXISTS reviews;
 
 CREATE TABLE reviews(
 
-reviewID INTEGER NOT NULL IDENTITY(0030000,2), -- PRIMARY KEY,
+entityId varchar(50), -- PRIMARY KEY,
 --creationDate DATETIME,
 --event Varchar(20), --example 192/168/1/1
-message text, -- 
+username Varchar(50),
+message TEXT, -- 
 star VARCHAR(20),
-imagepath VARCHAR(50),
+imagepath VARBINARY(MAX),
+datetime Varchar(50),
 
 -- primary key
-CONSTRAINT reviews_PK PRIMARY KEY(reviewID),
+CONSTRAINT reviews_PK PRIMARY KEY(entityId),
 
 );
 
-INSERT INTO reviews(message, star, imagepath) VALUES ('THIS IS A TEST', '0', null);
+INSERT INTO reviews(entityId, username, message, star, imagepath, datetime) VALUES (0,'Serge','THIS IS A TEST', '5', null,'2019');
+
+select * from reviews;
