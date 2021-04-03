50a98b5a6a93a6856f45780e92cb923c24e49d7a
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-04-02 19:44:17 -0700

    updated cypress

--- Async Logging/backend/ConsoleApp1/Program.cs
+++ Async Logging/backend/ConsoleApp1/Program.cs
@@ -11,6 +11,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            ReviewRatingDAO reviewRatingDAO = new ReviewRatingDAO("Server = localhost; Database = DB; Trusted_Connection = True;");
+            ReviewRatingService reviewRatingService = new ReviewRatingService(reviewRatingDAO);
+            ReviewRatingManager reviewRatingManager = new ReviewRatingManager(reviewRatingService);
+
+            reviewRatingManager.GetAllReviewsRatings();
+
+            var result = reviewRatingManager.GetReviewsRatings("30000");
+
             Console.WriteLine("Hello World");
 
             Console.Read();
