3bdecce40dee4efb342b245781328f80792e46ad
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-04-02 23:40:29 -0700

    added reviews and logger

---
+++ AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.Reviews.Apis/wwwroot/cypress/integration/examples/testing.spec.js
@@ -0,0 +1,63 @@
+/// <reference types="cypress" />
+
+/// <summary>
+/// References used from file: Solution Items/References.txt 
+/// N/A: some examples were referenced from the examples provided by cypress' default files.
+/// </summary>
+
+// This test block will test basic commands in the browser view
+context('Actions', () => {
+    beforeEach(() => {
+      cy.visit('https://localhost:44317/')
+    })
+
+    it('.type() - type into a DOM element', () => {
+
+        cy.get('#add-username')
+        .type('fake username').should('have.value', 'fake username')
+
+              // .type() with special character sequences
+        .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+        .type('{del}{selectall}{backspace}')
+
+        // .type() with key modifiers
+        .type('{alt}{option}') //these are equivalent
+        .type('{ctrl}{control}') //these are equivalent
+        .type('{meta}{command}{cmd}') //these are equivalent
+        .type('{shift}')
+
+        // Delay each keypress by 0.1 sec
+        .type('slow.typing@email.com', { delay: 100 })
+        .should('have.value', 'slow.typing@email.com')
+
+    })
+
+    it('.type() - type into a DOM element', () => {
+
+        cy.get('#add-message')
+        .type('fake message').should('have.value', 'fake message')
+
+              // .type() with special character sequences
+        .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+        .type('{del}{selectall}{backspace}')
+
+        // .type() with key modifiers
+        .type('{alt}{option}') //these are equivalent
+        .type('{ctrl}{control}') //these are equivalent
+        .type('{meta}{command}{cmd}') //these are equivalent
+        .type('{shift}')
+
+        // Delay each keypress by 0.1 sec
+        .type('fake message', { delay: 100 })
+        .should('have.value', 'fake message')
+
+    })
+
+    it('.clear() - clears an input or textarea element', () => {
+        // https://on.cypress.io/clear
+        cy.get('#add-message').type('Clear this text')
+          .should('have.value', 'Clear this text')
+          .clear()
+          .should('have.value', '')
+      })
+})
