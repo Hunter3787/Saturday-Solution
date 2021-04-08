983a2341206dfa58e73554769724197fa3b65eae
Farhatzee <46795280+Farhatzee@users.noreply.github.com>
2021-04-08 15:20:52 -0700

    added the icliams factory to security.interfaces

--- AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.Security/Interfaces/IClaimsFactory.cs
+++ AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.Security/Interfaces/IClaimsFactory.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using AutoBuildApp.Security.Enumerations;
+using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 
+
+/// <summary>
+/// https://www.dotnettricks.com/learn/designpatterns/factory-method-design-pattern-dotnet
+/// 
+/// </summary>
 namespace AutoBuildApp.Security.Interfaces
 {
-    interface IClaimsFactory
+
+    /// <summary>
+    /// the 'Product' interface
+    /// an interface for creating the objects
+    /// </summary>
+    public interface IClaimsFactory
     {
+        IEnumerable<Claim> Claims();
+        void PrintClaims();
+
     }
+
+
 }
+
