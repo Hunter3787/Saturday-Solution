cb792fd96d1c9498f58bdb4529f6c13ee2265347
Farhatzee <46795280+Farhatzee@users.noreply.github.com>
2021-04-08 15:15:46 -0700

    added ScopeEnumType.cs to the enumerations folder

---
+++ AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.Security/Enumerations/ScopeEnumType.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AutoBuildApp.Security.Enumerations
+{
+
+
+    public static class ScopeEnumType
+    {
+        public const string NONE = "NONE";
+        public const string ALL = "ALL";
+        public const string AUTOBUILD = "AUTOBUILD";
+        public const string SELF = "SELF";
+        public const string REVIEWS = "REVIEWS";
+        public const string SELF_REVIEWS = "SELF_REVIEWS";
+        public const string PRODUCTS = "PRODUCTS";
+        public const string VENDOR_PRODUCTS = "VENDOR_PRODUCTS";
+        public const string POST_BUILD = "POST_BUILD";
+        public const string UAD = "UAD";
+        public const string ADMIN_DASHBOARD = "ADMIN_DASHBOARD";
+        public const string ALL_USERS = "ALL_USERS";
+
+
+    }
+
+
+
+}
