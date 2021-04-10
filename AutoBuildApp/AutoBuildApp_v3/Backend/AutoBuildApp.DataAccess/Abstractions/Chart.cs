65e78fc0ee3ad4c73fe7079485774da1537997ac
Farhatzee <46795280+Farhatzee@users.noreply.github.com>
2021-04-10 14:50:45 -0700

    added code for the UAD

---
+++ AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.DataAccess/Abstractions/Chart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoBuildApp.DataAccess.Abstractions
+{
+    ///will be treating this as a base class
+    /// for now.
+    class Chart
+    {
+        /// <summary>
+        /// XLabel represents the
+        /// X Name Tick in the chart/ graph.
+        /// </summary>
+        public string XLabel { get; set; }
+        /// <summary>
+        /// Legend represents essentially 
+        /// the Z value in the chart.
+        /// </summary>
+        public string Legend { get; set; }
+        /// <summary>
+        /// YValue represent the corresponding
+        /// value to the X-Value.
+        /// </summary>
+        public int YValue { get; set; }
+
+
+
+
+
+    }
+}
