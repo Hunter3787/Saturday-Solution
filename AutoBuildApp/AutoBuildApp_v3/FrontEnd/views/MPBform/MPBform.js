aa5111e2e6697232c3e283bebfdccd10370b5aca
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-04-20 22:56:44 -0700

    made some more minor changes to frontend

--- AutoBuildApp/AutoBuildApp_v3/FrontEnd/views/MPBform/MPBform.js
+++ AutoBuildApp/AutoBuildApp_v3/FrontEnd/views/MPBform/MPBform.js
@@ -20,16 +20,31 @@ function addItem() {
     // Initializes a FormData object.
     let formData = new FormData();
 
+    // initialize an object for the build type value to be stored in.
+    let buildTypeValue;
+
     let title = document.getElementById('add-title'); // will get the value from the html element and store it.
     let username = document.getElementById('add-username'); // will get the value from the html element and store it.
     let description = document.getElementById('add-description'); // will get the value from the html element description and store it.
     let photo = document.getElementById("add-image").files[0]; // store the file in the photo variable.
+    let buildType = document.getElementsByName("build-type");
+
+    // This for loop will iterate through the radio elements and find which one is checked.
+    for (let i = 0; i < buildType.length; i++)
+    {
+        if (buildType[i].checked)
+        {
+            buildTypeValue = buildType[i].value
+        }
+    }
+
+    console.log(buildTypeValue);
 
     // The next 6 lines will store the above data in the formData object.
     formData.append("username", username.value.trim());
     formData.append("title", title.value.trim());
     formData.append("description", description.value.trim());
-    formData.append("buildType", 2);
+    formData.append("buildType", buildTypeValue);
     formData.append("buildImagePath", "C:/Test/Directory");
     formData.append("image", photo);
 
@@ -38,9 +53,9 @@ function addItem() {
 
     // Makes a fetch request to the controller with the specified attributes.
     fetch(uri, customRequest);
-    
 }
 
+// This function acts as a counter for the textarea field to show how many characters remain.
 function textCounter(field, field2, maxlimit) {
     var countfield = document.getElementById(field2);
 
