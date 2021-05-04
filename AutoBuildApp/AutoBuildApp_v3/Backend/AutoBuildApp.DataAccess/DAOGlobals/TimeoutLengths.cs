3e3b5b903173e989c0e42605c124fb3898208b1d
Farhatzee <46795280+Farhatzee@users.noreply.github.com>
2021-05-04 04:15:23 -0700

    -> no dao globals

--- AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.DataAccess/DAOGlobals/TimeoutLengths.cs
+++ AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.DataAccess/DAOGlobals/TimeoutLengths.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AutoBuildApp.DataAccess.DAOGlobals
+namespace AutoBuildApp.DataAccess //.DAOGlobals
 {
     public class TimeoutLengths
     {
