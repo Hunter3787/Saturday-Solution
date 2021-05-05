7e2f2795b8b643f7720f0bedd017f040abedb969
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-05-05 14:01:31 -0700

    Added a search feature for searching through build posts

--- AutoBuildApp/AutoBuildApp_v3/FrontEnd/views/MPBmain/MPB.js
+++ AutoBuildApp/AutoBuildApp_v3/FrontEnd/views/MPBmain/MPB.js
@@ -1,5 +1,6 @@
 const uri = 'https://localhost:5001/MostPopularBuilds/';
 let posts = [];
+let filterArray = [];
 let token = ' ';
 const fetchRequest = {
     method: 'GET',
@@ -10,6 +11,10 @@ const fetchRequest = {
         'Authorization': 'bearer ' + token
     }
 };
+
+// Adds an event listener for the key up event for a search filter.
+let searchFilter = document.getElementById('sbn');
+searchFilter.addEventListener("keyup", () => findByName(searchFilter));
 
 // Adds an event listener for redirecting to a different page when the post build button is clicked.
 let postBuild = document.getElementById('post-build-button');
@@ -24,6 +29,19 @@ let buildRadio = document.querySelectorAll('input[name="build"]').forEach((elem)
 let likesRadio = document.querySelectorAll('input[name="likes"]').forEach((elem) => {
   elem.addEventListener("change", () => getItems())
 })
+
+// This function conducts a filter that will filter the builds by search key.
+function findByName(filter)
+{
+  // sets the array equal to the entire array of posts
+  let filterArray = posts;
+
+  // sets a result equal to the filtered array by the passed in parameter
+  let result = filterArray.filter(item => item.title.toLowerCase().includes(filter.value.toLowerCase()) || item.username.toLowerCase().includes(filter.value.toLowerCase()))
+
+  // calls the display filtered data array function.
+  displayFilteredData(result);
+}
 
 // This function gets the filter string if radio buttons are clicked.
 function getFilterString()
@@ -88,6 +106,7 @@ async function getItems() {
     await fetch(uri + getFilterString(), fetchRequest) // fetches the default URI
         .then(response => response.json()) // Will receive a response from the default response.json.
         .then(data => displayItems(data)) // will call the display items function.
+        .then(() => findByName(searchFilter))
         .then(console.log("reloaded"))
         .catch(error => console.error('Unable to get items.', error)); // will catch an error and print the appropriate error message in console.
     refreshData;
@@ -182,6 +201,98 @@ function displayItems(data) {
     });
 
   posts = data; // will store the data as an array in this variable for local access.
+}
+
+
+// This function will display items received from the filter data function.
+function displayFilteredData(data) {
+
+  const innerDiv = document.getElementById('main'); // This will get the id of the form from the HTML.
+  innerDiv.innerHTML = ''; // appends a null value to the inner HTML, as is not required.
+
+  // This function will create a table, and append values for each column and iterate to the next row of items.
+  data.forEach(item => {
+
+      // These are the HTML elements that will be created by the following code.
+
+      //     <div class="gridbuilds">
+      //      <div class="blockbuild" onclick="location.href='../MPBpost/MPBpost.html';">
+      //         <p>Title</p>
+      //         <div class="buildimage">
+      //             <img src="http://cdna.pcpartpicker.com/static/forever/images/userbuild/358512.a97c3b2732e2a4d83247e1105f455c63.512.jpg">
+      //         </div>
+      //         <p>Username</p>
+      //         <p>Likes</p>
+      //         <p>Build Type</p>
+      //      </div>
+      //     </div>
+
+      // make the div for the grid of builds
+      var gridbuilds = document.createElement('div');
+      gridbuilds.classList.add('gridbuilds');
+
+      // make the div for each individual build and append it to the build block.
+      var blockbuild = document.createElement('div');
+      blockbuild.classList.add('blockbuild');
+
+      // lambda function for redirecting on click.
+      blockbuild.addEventListener("click", () => {
+        sessionStorage.setItem('buildId', item["entityId"]);
+        window.location.assign("../MPBpost/MPBpost.html")
+      }); 
+
+      // create a title element, appends text to it, and then appends all to a build block.
+      var title = document.createElement('p');
+      var titletext = document.createTextNode("Title: "+ item["title"]);
+      title.appendChild(titletext);
+      blockbuild.appendChild(title);
+
+      // create the div element that encapsulates the image and then appends all to a block build.
+      var buildimage = document.createElement('div');
+      buildimage.classList.add('buildimage');
+      var image = document.createElement("img");
+      image.src = item["buildImagePath"];
+      buildimage.appendChild(image);
+      blockbuild.appendChild(buildimage);
+
+      // creates the username element, appends text to it, and then appends all to a build block.
+      var username = document.createElement('p');
+      var usernametext = document.createTextNode("User: " + item["username"]);
+      username.appendChild(usernametext);
+      blockbuild.appendChild(username);
+
+      // creates the likes element, appends text to it, and then appends all to a build block.
+      var likes = document.createElement('p');
+      var likestext = document.createTextNode("Likes: " + item["likeIncrementor"]);
+      likes.appendChild(likestext);
+      blockbuild.appendChild(likes);
+
+      // creates the build type element, appends text to it, and then appends all to a build block.
+      var buildtype = document.createElement('p');
+
+
+      var build = "None";
+
+      if (item["buildType"] === 1)
+        build = "Graphic Artist";
+      else if (item["buildType"] === 2)
+        build = "Gaming";
+      else if (item["buildType"] === 3)
+        build = "Word Processing";
+
+
+      var buildtypetext = document.createTextNode("Build: " + build);
+      buildtype.appendChild(buildtypetext);
+      blockbuild.appendChild(buildtype);
+
+      // appends the block to the grid of builds.
+      gridbuilds.appendChild(blockbuild);
+
+      // appends the grid of builds to the main
+      innerDiv.appendChild(gridbuilds);
+  });
+
+  filterArray = data; // will store the data as an array in this variable for local access.
 }
 
 //let initializeView = document.getElementById("initialize-view").innerHTML = getItems();
