9d27a66273d8d93a560815ff3d2e6bfe6fd6eab8
crkobel <crkobel@verizon.net>
2021-05-06 16:06:30 -0700

    tried getting email verification to work

--- AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.Api/Controllers/RegistrationController.cs
+++ AutoBuildApp/AutoBuildApp_v3/Backend/AutoBuildApp.Api/Controllers/RegistrationController.cs
@@ -8,6 +8,10 @@ using AutoBuildApp.Managers;
 using AutoBuildApp.Services.UserServices;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using AutoBuildApp.Models.Users;
+using AutoMapper;
+using AutoBuildApp.Services.EmailService;
 
 namespace AutoBuildApp.Api.Controllers
 {
@@ -16,20 +20,51 @@ namespace AutoBuildApp.Api.Controllers
     [ApiController]
     public class RegistrationController : Controller
     {
+        //private readonly IMapper _mapper;
+        //private readonly UserManager<User> _userManager;
+        //private readonly SignInManager<User> _signInManager;
+        //private readonly IEmailSender _emailSender;
+
+        //public RegistrationController(IMapper mapper, UserManager<User> userManager, SignInManager<User> signInManager, IEmailSender emailSender)
+        //{
+        //    _mapper = mapper;
+        //    _userManager = userManager;
+        //    _signInManager = signInManager;
+        //    _emailSender = emailSender;
+        //}
         RegistrationManager _registrationManager = new RegistrationManager(ConnectionManager.connectionManager.GetConnectionStringByName("MyConnection"));
 
         [HttpPost]
+        //public async Task<IActionResult> RegisterUser(string username, string firstname, string lastname, string email, string password,
+        //    string passwordCheck)
         public IActionResult RegisterUser(string username, string firstname, string lastname, string email, string password,
             string passwordCheck)
         {
-            //username = "spiderman";
-            //firstname = "Peter";
-            //lastname = "Parker";
-            //email = "spiderman@gmail.com";
-            //password = "Password123";
-            //passwordCheck = "Password123";
+            //RegisterUser registerUser = new RegisterUser();
+            //registerUser.Email = email;
+            //var user = _mapper.Map<User>(registerUser);
+
+            //var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            //var confirmationLink = Url.Action(nameof(ConfirmEmail), "Account", new { token, email = user.Email }, Request.Scheme);
+
+            //var message = new Message(new string[] { user.Email }, "Confirmation email link", confirmationLink, null);
+            //await _emailSender.SendEmailAsync(message);
+
+            //await _userManager.AddToRoleAsync(user, "Visitor");
+
 
             return Ok(_registrationManager.RegisterUser(username, firstname, lastname, email, password, passwordCheck));
         }
+
+        //[HttpGet]
+        //public async Task<IActionResult> ConfirmEmail(string token, string email)
+        //{
+        //    var user = await _userManager.FindByEmailAsync(email);
+        //    if (user == null)
+        //        return View("Error");
+
+        //    var result = await _userManager.ConfirmEmailAsync(user, token);
+        //    return View(result.Succeeded ? nameof(ConfirmEmail) : "Error");
+        //}
     }
 }
