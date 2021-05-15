f5423df79fea62de1e5738b68d3ac50e383bfe93
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-05-15 03:15:45 -0700

    fixed redundancy

--- AutoBuildApp/AutoBuildApp_v3/FrontEnd/config.js
+++ AutoBuildApp/AutoBuildApp_v3/FrontEnd/config.js
@@ -3,7 +3,7 @@ const appConfigurations =
 {
     Endpoints: {
         MostPopularBuilds: "http://ec2-13-52-186-63.us-west-1.compute.amazonaws.com:8081/MostPopularBuilds/",
-        ReviewsRatings: "http://http://ec2-13-52-186-63.us-west-1.compute.amazonaws.com:8081/reviewrating",
-        default: "http://http://ec2-13-52-186-63.us-west-1.compute.amazonaws.com:8081/Home/Error"
+        ReviewsRatings: "http://ec2-13-52-186-63.us-west-1.compute.amazonaws.com:8081/reviewrating",
+        default: "http://ec2-13-52-186-63.us-west-1.compute.amazonaws.com:8081/Home/Error"
     }
 }
